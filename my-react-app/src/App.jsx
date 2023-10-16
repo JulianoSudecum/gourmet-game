2a335d963d559fd13a4b56a6b6ff23696405b119
Juliano <julianosudecum525@gmail.com>
2023-10-16 15:09:51 -0300

    feat: gourmetgame v1.0

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css'
+
+function App() {
+
+  const foodOptions = ["Massa", "Lasanha"]
+
+  const quiz = () => {
+    let stopIteration = false;
+  
+    foodOptions.map((food) => {
+      if (stopIteration) return;
+  
+      let response = prompt(`O prato que você pensou é ${food}?`);
+  
+      if (response.toLocaleLowerCase() === "sim") {
+        alert("Acertei dnv!");
+        stopIteration = true;
+      }
+    });
+
+    if(!stopIteration){
+      let correctFood = prompt("Qual prato você pensou?")
+      let foodDiference = prompt(`${correctFood} é ______ mas Lasanha não.`)
+      alert("Fim do questionario! Obrigado pela atenção :)")
+      return foodDiference
+    }
+  };
+
+  return (
+    <>
+      <img className='logo' src="src\assets\conteudize.jpg" alt="" />
+      <div className='mainDiv'>
+        <p>Pense em um prato que gosta</p>
+        <button onClick={quiz}>Iniciar</button>
+      </div>
+      <p id='credits'>created by Juliano Sudecum</p>
+    </>
+  )
+}
+
+export default App
