bd0c88dc902f00758ecf919fbda170ae44154c7c
Juliano <julianosudecum525@gmail.com>
2023-10-20 01:48:27 -0300

    feat: GourmetGame Final

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,97 +1,115 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
+  const plates = [
+    {
+      name: "Lasanha",
+      characteristics: ["massa"],
+    },
+    {
+      name: "Macarrão",
+      characteristics: ["massa", "cozido"],
+    },
+    {
+      name: "Brigadeiro",
+      characteristics: ["doce"],
+    },
+  ];
   const quiz = () => {
-  
-    const pratos = [
-      {
-        "nome": "Lasanha",
-        "caracteristicas": ["massa"]
-      },
-      {
-        "nome": "Macarrão",
-        "caracteristicas": ["massa", "cozido"]
-      },
-      {
-        "nome": "Brigadeiro",
-        "caracteristicas": ["doce"]
-      }
-    ];
 
-    let array = pratos.map((prato) => prato.caracteristicas);
-    let isFinished = false
+    let array = plates.map((plate) => plate.characteristics);
+    let isFinished = false;
 
     let uniqueArray = [...new Set(array.flat())];
 
     uniqueArray.map((element, index) => {
-      console.log(index)
-      if(!isFinished){
-        let response = prompt(`O prato que você pensou é ${element}?`)
-      // efetua o filtro de separacao por caracteristicas
-      if(response.toLowerCase() == "sim"){
-        let arrayFiltered = pratos.filter((pratoFilter) => pratoFilter.caracteristicas == element)
-        pratos.map((prato) => {
-          if(prato.caracteristicas.length >= 2){
-            prato.caracteristicas.map((caracter) => {
-              if(caracter == element){
-                arrayFiltered.push(prato)
-              }
-            })
+      if (!isFinished) {
+        let response = prompt(`O prato que você pensou é ${element}?`);
+        if (response.toLowerCase() == "sim") {
+          let arrayFiltered = plates.filter(
+            (plateFilter) => plateFilter.characteristics == element
+          );
+          plates.map((plate) => {
+            if (plate.characteristics.length >= 2) {
+              plate.characteristics.map((singleCharacteristic) => {
+                if (singleCharacteristic == element) {
+                  arrayFiltered.push(plate);
+                }
+              });
+            }
+          });
+          if (arrayFiltered.length <= 1) {
+            let response = prompt(
+              `O prato que você pensou é ${arrayFiltered[0].name}?`
+            );
+            if (response.toLowerCase() == "sim") {
+              alert("Acertei denovo!");
+              isFinished = true;
+            }
           }
-        })
-        // condicional se o elemento tiver apenas uma caracteristica
-        if(arrayFiltered.length <= 1){
-          console.log(arrayFiltered)
-          let response = prompt(`O prato que você pensou é ${arrayFiltered[0].nome}?`)
-          if(response.toLowerCase() == "sim"){
-            alert("Acertei denovo!")
-            isFinished = true
-          }
-        }
-        // caso elemento tiver +1 caracteristica
-        // verificar se é cozido antes que perguntar o prato
-        else{
-          arrayFiltered.map((prato) => {
-            prato.caracteristicas.map((caracteristica) => {
-              if(caracteristica != element){
-                const index = arrayFiltered.filter((filtered, index) => filtered.caracteristicas[index] == caracteristica)
-                console.log(caracteristica)
-                let response = prompt(`O prato que você pensou é ${caracteristica}?`)
-                if(response.toLowerCase() == "sim"){
-                  console.log(index)
-                  let response = prompt(`O prato que você pensou é ${index[0].nome}`)
-                  if(response.toLowerCase() == "sim"){
-                    alert("Acertei denovo!")
-                    isFinished = true
-                  }
-                } else {
-                  const array = arrayFiltered.filter((arrayElement) => arrayElement.nome != index[0].nome)
-                  let response = prompt(`O prato que você pensou é ${array[0].nome}?`)
-                  if(response.toLowerCase() == "sim"){
-                    alert("Acertei denovo!")
-                    isFinished = true
+          else if (arrayFiltered.length > 1) {
+            arrayFiltered.map((plate) => {
+              plate.characteristics.map((characteristic) => {
+                if (characteristic != element) {
+                  const index = arrayFiltered.filter(
+                    (filtered, index) =>
+                      filtered.characteristics[index] == characteristic
+                  );
+                  let response = prompt(
+                    `O prato que você pensou é ${characteristic}?`
+                  );
+                  if (response.toLowerCase() == "sim") {
+                    let response = prompt(
+                      `O prato que você pensou é ${index[0].name}`
+                    );
+                    if (response.toLowerCase() == "sim") {
+                      alert("Acertei denovo!");
+                      isFinished = true;
+                    }
+                  } else {
+                    const array = arrayFiltered.filter(
+                      (arrayElement) => arrayElement.name != index[0].name
+                    );
+                    let response = prompt(
+                      `O prato que você pensou é ${array[0].name}?`
+                    );
+                    if (response.toLowerCase() == "sim") {
+                      alert("Acertei denovo!");
+                      isFinished = true;
+                    }
                   }
                 }
-              }
-            })
-          })
+              });
+            });
+          }
         }
-      }}
-    })
-    }
-    // caso nao seja nenhum do armazenamento criar excecao para outro prato
+        else {
+          if((index + 1) == plates.length){
+            let plateResponse = prompt("Qual prato você pensou?");
+            let characteristicResponse = prompt(
+              `${plateResponse} é _______ mas `
+            );
+            let newPlate = {
+              name: plateResponse,
+              characteristics: [characteristicResponse],
+            }
+            plates.push(newPlate)
+          }
+        }
+      }
+    });
+  };
 
   return (
     <>
-      <img className='logo' src="conteudize.jpg" alt="" />
-      <div className='mainDiv'>
+      <img className="logo" src="conteudize.jpg" alt="" />
+      <div className="mainDiv">
         <p>Pense em um prato que gosta</p>
         <button onClick={quiz}>Iniciar</button>
       </div>
-      <p id='credits'>created by Juliano Sudecum</p>
+      <p id="credits">created by Juliano Sudecum</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
