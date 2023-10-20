5694daa8b5ddcedf63fe2a90679f7d8333608880
Juliano <julianosudecum525@gmail.com>
2023-10-19 22:48:20 -0300

    save

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -79,69 +79,6 @@ function App() {
         }
       }
     })
-
-    // let caracteristicas = pratos.map((prato) => prato.caracteristicas);
-    // caracteristicas = caracteristicas.filter((element) => {
-    //   const found = caracteristicas.includes(element);
-    //   if(!found){
-    //     caracteristicas.push(element);
-    //   }
-    // })
-
-    // console.log(caracteristicas.flat().uniq())
-
-
-    // caracteristicas.map((caracteristica) => {
-    //   caracteristica.map((element) => {
-    //     let response = prompt(`O prato que você pensou é ${element}?`)
-        
-    //   })
-    // })
-    
-      // let prato = pratos.find((p) => p.caracteristicas.includes("massa"));
-    
-      // if (!prato) {
-      //   return null;
-      // }
-    
-      // let pergunta = "O prato que você pensou é uma massa?";
-      // let resposta = window.prompt(pergunta);
-    
-      // if (resposta === "sim") {
-      //   pergunta = "O prato que você pensou é lasanha?";
-      //   resposta = window.prompt(pergunta);
-    
-      //   if (resposta === "sim") {
-      //     return prato;
-      //   } else {
-      //     pratos.map((item) => {
-      //       if(item.nome != prato.nome){
-      //         item.caracteristicas.map((caracteristica) => {
-      //           if(caracteristica == "massa"){
-      //             let pergunta = window.prompt(`O prato que você pensou é uma ${item.nome}?`)
-
-      //             if(pergunta == "sim"){
-      //               console.log(`O prato que você pensou foi ${item.nome}`)
-      //               return item
-      //             }
-      //             else if(pergunta == "nao"){
-      //               let nome = window.prompt("Qual prato você pensou?")
-      //               let caracteristica = window.prompt("Fale uma caracteristica do seu prato: ")
-      //               const novoPrato = {
-      //                 nome,
-      //                 caracteristica
-      //               }
-      //               pratos.push(novoPrato)
-      //               console.log(pratos)
-      //             }
-      //           }
-      //         })
-      //       }
-      //     })
-      //   }
-      // } else {
-      //   return null;
-      // }
     }
 
   return (
